fabbd81746972ed0f6790a30cabfec90e78b7600
HISUIE <32667550+hisuie08@users.noreply.github.com>
2020-10-23 16:40:39 +0900

    修正

--- getter.py
+++ getter.py
@@ -1,6 +1,9 @@
-import requests
 import base64
+import json
 import os
+
+import requests
+
 PATH = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -12,15 +15,15 @@ def load_git_token():
 GITHUB_TOKEN = load_git_token()
 
 
-def getter(org, repo, branch):
-    branch = "main"  # デフォルトブランチ名master時代のリポジトリは注意
-    url = f"http://api.github.com/repos/{org}/{repo}/content/bot.json "
-    res = requests.get(url, headers={
-                       'Authorization': f'token {GITHUB_TOKEN}'}, params={"ref": branch})
+def getter(org: str, repo: str, branch="main") -> json:
+    # デフォルトブランチ名master時代のリポジトリは注意
+    url = f"http://api.github.com/repos/{org}/{repo}/contents/bot.json"
+    headers = {
+        'Authorization': f'token {GITHUB_TOKEN}'
+    }
+    params = {"ref": branch}
+    res = requests.get(url, headers=headers, params=params)
     res.raise_for_status()
-    base64Data = res.json()["content"]
-    print(base64Data)
-    return
-
-
-getter()
+    rawBotData = str(res.json()["content"]).replace("\n", "")
+    jsonBotData = json.loads(base64.b64decode(rawBotData).decode("utf-8"))
+    return jsonBotData
