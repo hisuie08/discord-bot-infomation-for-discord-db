bc09ee25bd7f49d57d0903a3ea8e77a8b1c0b541
HISUIE <32667550+hisuie08@users.noreply.github.com>
2020-10-23 18:26:21 +0900

    DocString追加

--- getter.py
+++ getter.py
@@ -16,7 +16,27 @@ GITHUB_TOKEN = load_git_token()
 
 
 def getter(org: str, repo: str, branch="main") -> json:
-    # デフォルトブランチ名master時代のリポジトリは注意
+    """
+    Githubからbot.jsonを取得、パースする
+
+    Params:
+    --------
+    org : str
+        Githubのユーザー名
+
+    repo : str
+        bot.jsonがあるリポジトリ
+
+    branch : str
+        デフォルトブランチ(main)以外を指定したいときに指定。
+        デフォルトブランチ名master時代のリポジトリなど
+
+    Returns:
+    --------
+    jsonBotData : json
+        bot.jsonの中身
+
+    """
     url = f"http://api.github.com/repos/{org}/{repo}/contents/bot.json"
     headers = {
         'Authorization': f'token {GITHUB_TOKEN}'
